dc74e8489e5d0ca5bcca83d9fa4a4cffe1d3109a
mip <smetaninPavel.rus@gmail.com>
2015-09-12 08:49:05 +0500

    create profile settings v 0.0.1

--- public/js/profileSettingsF.js
+++ public/js/profileSettingsF.js
@@ -3,4 +3,38 @@
 
 $(document).ready(function() {
 
+	var mainSettings = $('#mainSettings'),
+		personalSettings = $('#personalSettings'),
+		additionalSettings = $('#additionalSettings');
+
+	mainSettings.click(function() {
+		var settingsContent = $(this).children('.settings-content');
+
+		if (settingsContent.css('display') == 'none') {
+			settingsContent.show();
+		} else {
+			settingsContent.hide();
+		}
+	});
+
+	personalSettings.click(function() {
+		var settingsContent = $(this).children('.settings-content');
+
+		if (settingsContent.css('display') == 'none') {
+			settingsContent.show();
+		} else {
+			settingsContent.hide();
+		}
+	});
+
+	additionalSettings.click(function() {
+		var settingsContent = $(this).children('.settings-content');
+
+		if (settingsContent.css('display') == 'none') {
+			settingsContent.show();
+		} else {
+			settingsContent.hide();
+		}
+	});
+
 });
