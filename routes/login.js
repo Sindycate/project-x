130f15aa5b8c3ff0a0ac1553ad58137cbfdbe365
mip <smetaninPavel.rus@gmail.com>
2015-09-19 00:14:02 +0500

    v 0.1.1

--- routes/login.js
+++ routes/login.js
@@ -1,36 +1,42 @@
-var express = require('express'),
+	var express = require('express'),
 	router = express.Router(),
 	mysql = require('mysql'),
-	crypto = require('crypto'),
-
-connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : '2177721',
-	database : 'test'
-});
-
-connection.connect(function(err) {
-	if(!err) {
-		console.log("Database is connected ... \n\n");
-	} else {
-		console.log("Error connecting database ... \n\n");
-	}
-});
+	crypto = require('crypto');
 
 router.get('/', function(req, res, next) {
 	if (req.session.user) {
 		res.redirect('/');
 	} else {
-		res.render('login', {success: false});
+		res.render('login', {});
 	}
 });
 
 
-function checkForDuplicates(post, callback) {
 
+router.post('/', function(req, res) {
+	var post = req.body;
+
+	if (!(post.password && post.login)) {
+		res.json({success: false, message: 'Вы заполнили не все поля'});
+		return;
+	}
+	post.password = passwordHash(post.password);
+
+	checkForDuplicates(post, function (result, user) {
+		console.log('test');
+		if (result) {
+			req.session.user = user.id;
+			req.session.login = user.login;
+			res.json({success: true, userLogin: user.login});
+		} else {
+			res.json({success: false, message: 'Логин или пароль не подходят'});
+			return;
+		}
+	});
+});
+
+function checkForDuplicates(post, callback) {
 	connection.query('SELECT * from users where login = ? and password = ?', [post.login, post.password], function(err, rows, fields) {
-		// connection.end();
 		if (!err) {
 			console.log(rows);
 			if(rows != '') {
@@ -48,26 +54,5 @@ function passwordHash(password) {
 	password = crypto.createHmac('sha1', '').update(password).digest('hex');
 	return password;
 };
-
-router.post('/', function(req, res) {
-	var post = req.body;
-
-	if (!(post.password && post.login)) {
-		res.render('login', {success: false, message: 'Вы заполнили не все поля'});
-	}
-	post.password = passwordHash(post.password);
-	console.log(post.password);
-
-	checkForDuplicates(post, function (result, user) {
-		if (result) {
-			req.session.user = user.id;
-			req.session.login = user.login;
-			// res.render('login', {success: true, message: 'Вы успешно авторизировались!'});
-			res.redirect('/' + user.login);
-		} else {
-			res.render('login', {success: false, message: 'Логин или пароль неверны'});
-		}
-	});
-});
 
 module.exports = router;
